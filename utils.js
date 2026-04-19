0d35911bd1a2648609fc457a2e1dc303d613a158
Gryzax <fourkane.ahmer-elain@epitech.eu>
2026-04-19 21:15:28 +0200

    add: update ci

--- utils.js
+++ utils.js
@@ -1,26 +1,26 @@
 class Vec {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    add(v) {
-        return new Vec(this.x + v.x, this.y + v.y);
-    }
-    mul(x, y) {
-        var y = y || x;
-        return new Vec(this.x * x, this.y * y);
-    }
-    dot(v) {
-        return this.x * v.x + this.y * v.y;
-    }
-    cross(v) {
-        return this.x * v.y - v.x * this.y;
-    }
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  add(v) {
+    return new Vec(this.x + v.x, this.y + v.y);
+  }
+  mul(x, y) {
+    var y = y || x;
+    return new Vec(this.x * x, this.y * y);
+  }
+  dot(v) {
+    return this.x * v.x + this.y * v.y;
+  }
+  cross(v) {
+    return this.x * v.y - v.x * this.y;
+  }
 }
 function getTimeStr(dt) {
-    let timeMs = `${Math.floor((dt % 1000) / 10)}0`.slice(0, 2);
-    let timeS = `0${Math.floor(dt / 1000) % 60}`.slice(-2);
-    let timeM = Math.floor(dt / 1000 / 60);
-    return `${timeM}:${timeS}.${timeMs}`;
+  const timeMs = `${Math.floor((dt % 1000) / 10)}0`.slice(0, 2);
+  const timeS = `0${Math.floor(dt / 1000) % 60}`.slice(-2);
+  const timeM = Math.floor(dt / 1000 / 60);
+  return `${timeM}:${timeS}.${timeMs}`;
 }
-module.exports = { Vec, getTimeStr };
+module.exports = {Vec, getTimeStr};
